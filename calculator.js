b5654f01efca9e1e894f539f596378124d278799
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-11-04 14:37:29 -0500

    Write all functions in shorthand notation

--- calculator.js
+++ calculator.js
@@ -1,19 +1,8 @@
 // Calculator JS //
 
-// const add = (a, b) => sum = (a + b);
-
 let fixedNum = 6;
 
-const roundAns = function(ans) {
-    if (Number.isInteger(ans)) {
-        return ans;
-    } else {
-        let round = 0.0;
-        return round = Number(ans.toFixed(fixedNum));
-    }
-}
-
-// const roundAns = ans => Number.isInteger(ans) ? ans : Number(ans.toFixed(fixedNum));
+const roundAns = ans => (Number.isInteger(ans)) ? ans : Number(ans.toFixed(fixedNum));
 
 const add = (a, b) => roundAns(a + b);
 
@@ -29,5 +18,3 @@ console.log(subtract(6.666666666, 9999.999999));
 console.log(multiply(2, 3));
 console.log(divide(3, 2));
 
-// rounding positive numbers
-// Number(ans)
