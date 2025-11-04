7666ffd687d93b6fafbb40cbe46e68ce192f9d0e
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-11-04 14:30:31 -0500

    Add math functions & rounding decimal places fn

--- calculator.js
+++ calculator.js
@@ -1,1 +1,33 @@
 // Calculator JS //
+
+// const add = (a, b) => sum = (a + b);
+
+let fixedNum = 6;
+
+const roundAns = function(ans) {
+    if (Number.isInteger(ans)) {
+        return ans;
+    } else {
+        let round = 0.0;
+        return round = Number(ans.toFixed(fixedNum));
+    }
+}
+
+// const roundAns = ans => Number.isInteger(ans) ? ans : Number(ans.toFixed(fixedNum));
+
+const add = (a, b) => roundAns(a + b);
+
+const subtract = (a, b) => roundAns(a - b);
+
+const multiply = (a, b) => roundAns(a * b);
+
+const divide = (a, b) => roundAns(a / b);
+
+console.log(add(3.33333, 3.666666));
+console.log(add(-4.77777, -9.999999));
+console.log(subtract(6.666666666, 9999.999999));
+console.log(multiply(2, 3));
+console.log(divide(3, 2));
+
+// rounding positive numbers
+// Number(ans)
