f8565695d20e1e282d1da26f5497f69d23b0c6d5
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-11-10 12:43:05 -0500

    Initial styling of calculator display

--- calculator.js
+++ calculator.js
@@ -1,9 +1,8 @@
 // Calculator JS //
 
 let fixedNum = 6;
-
-const roundAns = ans => (Number.isInteger(ans)) ? ans : Number(ans.toFixed(fixedNum));
-
+// let a = 0;  assign this value with an input/event listener
+// let b = 0;  assign this value with an input/event listener
 const add = (a, b) => roundAns(a + b);
 
 const subtract = (a, b) => roundAns(a - b);
@@ -12,9 +11,32 @@ const multiply = (a, b) => roundAns(a * b);
 
 const divide = (a, b) => roundAns(a / b);
 
-console.log(add(3.33333, 3.666666));
-console.log(add(-4.77777, -9.999999));
-console.log(subtract(6.666666666, 9999.999999));
-console.log(multiply(2, 3));
-console.log(divide(3, 2));
+const roundAns = ans => (Number.isInteger(ans)) ? ans : Number(ans.toFixed(fixedNum));
+
+let operator = {
+    addition: "+",
+    subtraction: "-",
+    multiplication: "*",
+    division: "/",
+}
+
+let operate = function(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    }   else if (operator === "/") {
+        return divide(a, b);
+    }
+}
+
+console.log(operate("/", 4, 6));
+
+// console.log(add(3.33333, 3.666666));
+// console.log(add(-4.77777, -9.999999));
+// console.log(subtract(6.666666666, 9999.999999));
+// console.log(multiply(2, 3));
+// console.log(divide(3, 2));
 
