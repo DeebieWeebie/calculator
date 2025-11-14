2cbcc4d0a3b3e8fff1ea817debb17c49a7e3f38e
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-11-13 19:34:00 -0500

    Add event listeners to numbers and operators

--- calculator.js
+++ calculator.js
@@ -1,8 +1,37 @@
 // Calculator JS //
 
 let fixedNum = 6;
-// let a = 0;  assign this value with an input/event listener
-// let b = 0;  assign this value with an input/event listener
+let a = 0;  // assign this value with an input/event listener
+let b = 0;  // assign this value with an input/event listener
+
+const inputLeft = document.querySelector(".input__left");
+const inputRight = document.querySelector(".input__right");
+
+// count the number of clicks?  If inputLeft = 1, operator = 1, then set b?
+//  for larger digits, keep reassigning a by adding new digit press to a
+// ADD CLICKCOUNTS AND EQUALITY NEXT
+
+inputLeft.addEventListener("click", function(event) {
+    if (event.target.classList.contains("input__numeric")) {
+        const number = event.target.value;
+        a = parseFloat(number);
+    // } else if (event.target.classList.contains("input__decimal")) {
+    //     const decimal = event.target.value;
+    // } else if (event.target.class.contains("input__backspace")) {
+        // something about removing the last part of the digit
+    }
+    console.log(a);
+    console.log(typeof(a));
+});
+
+inputRight.addEventListener("click", function(event) {
+    if (event.target.classList.contains("input__operator")) {
+        operator = event.target.value;
+    }
+    console.log(operator);
+});
+
+
 const add = (a, b) => roundAns(a + b);
 
 const subtract = (a, b) => roundAns(a - b);
