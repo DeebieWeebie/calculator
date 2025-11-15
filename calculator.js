aa3f75c975afb476fdbee39e6414aa3e1f754a9e
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-11-15 15:16:05 -0500

    Add a boolean variable & refactor event listeners
    A boolean variable needs to be set as true after
    an operator is pressed so that the second digit
    can be started.
    
    Refactor eventListeners to call digit, dot, clear,
    backspace, & operator handling functions that use
    this boolean value to display the correct digits.

--- calculator.js
+++ calculator.js
@@ -1,36 +1,77 @@
 // Calculator JS //
 
-let fixedNum = 6;
-let a = 0;  // assign this value with an input/event listener
-let b = 0;  // assign this value with an input/event listener
+let firstNumber = null;  
+let operator = null;
+let displayValue = "";
+let isReadyForNewInput = false;  // set to true after the operator is pressed
 
+const displayWindow = document.querySelector(".display__window");
 const inputLeft = document.querySelector(".input__left");
 const inputRight = document.querySelector(".input__right");
+const inputBottom = document.querySelector(".input__bottom");
+const clearButton = document.querySelector(".display__clear");
 
-// count the number of clicks?  If inputLeft = 1, operator = 1, then set b?
-//  for larger digits, keep reassigning a by adding new digit press to a
-// ADD CLICKCOUNTS AND EQUALITY NEXT
+function updateDisplay() {
+    if (displayValue === "") {
+        displayWindow.textContent = "0";  // user always sees 0 when calulator 
+    } else {                                // is clear or reloads
+        displayWindow.textContent = displayValue;
+    }
+}
+
+function handleInputDigit(digit) {
+    if (isReadyForNewInput === true) {
+        displayValue = digit;
+        isReadyForNewInput === false;
+    } else {                            // stops the leading 0 on the digit
+        if (displayValue === "0") {
+            displayValue = digit;
+        } else {
+            displayValue + digit;
+        }
+    }
+    updateDisplay();
+}
+
+function handleInputDecimal(dot) {
+    if (isReadyForNewInput === true) {
+        alert("When entering a non-integer value like 0.2, you must type the zero first")
+        return;                         // if an operator has been pressed & 
+    }                                   // then a decimal, the user is asked to
+                                         // type a zero first and the code exits
+    if (!displayValue.includes(dot)) {
+        displayValue += dot;
+    }
+    updateDisplay();
+}
+
+// function handleBackspace() {
+
+// }
 
 inputLeft.addEventListener("click", function(event) {
     if (event.target.classList.contains("input__numeric")) {
-        const number = event.target.value;
-        a = parseFloat(number);
-    // } else if (event.target.classList.contains("input__decimal")) {
-    //     const decimal = event.target.value;
-    // } else if (event.target.class.contains("input__backspace")) {
-        // something about removing the last part of the digit
+        handleInputDigit(event.target.value);
+    } else if (event.target.classList.contains("input__decimal")) {
+        handleInputDecimal(event.target.value);
+    } else if (event.target.classList.contains("input__backspace")) {
+        handleBackspace();
     }
-    console.log(a);
-    console.log(typeof(a));
 });
 
 inputRight.addEventListener("click", function(event) {
     if (event.target.classList.contains("input__operator")) {
-        operator = event.target.value;
+        handleOperator(event.target.value);
     }
-    console.log(operator);
 });
 
+inputBottom.addEventListener("click", function(event) {
+    if (event.target.classList.contains("input__equality")) {
+        handleOperator(event.target.value);
+    }
+});
+
+updateDisplay();
 
 const add = (a, b) => roundAns(a + b);
 
@@ -41,13 +82,6 @@ const multiply = (a, b) => roundAns(a * b);
 const divide = (a, b) => roundAns(a / b);
 
 const roundAns = ans => (Number.isInteger(ans)) ? ans : Number(ans.toFixed(fixedNum));
-
-let operator = {
-    addition: "+",
-    subtraction: "-",
-    multiplication: "*",
-    division: "/",
-}
 
 let operate = function(operator, a, b) {
     if (operator === "+") {
@@ -69,3 +103,7 @@ console.log(operate("/", 4, 6));
 // console.log(multiply(2, 3));
 // console.log(divide(3, 2));
 
+    // } else if (event.target.classList.contains("input__decimal")) {
+    //     const decimal = event.target.value;
+    // } else if (event.target.class.contains("input__backspace")) {
+        // something about removing the last part of the digit
